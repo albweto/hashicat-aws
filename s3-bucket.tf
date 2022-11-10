0e0afb0c22e09159a62214d9ddae8ba8447833e5
Samuel Henriquez <samualbwt@gmail.com>
2022-11-10 19:14:02 +0000

    prueba de subida

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,7 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.8.0"
+  # insert the 5 required variables here
+    bucket_prefix = "hashicat-samuel"
 }

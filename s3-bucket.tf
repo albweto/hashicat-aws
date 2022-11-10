bc5fd06d89ebd7ce5fe5d6f1f266a3c8e02daf72
Samuel Henriquez <samualbwt@gmail.com>
2022-11-10 19:31:54 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,5 +3,5 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
   # insert the 5 required variables here
-    bucket_prefix = "hashicat-samuel"
+    bucket_prefix = "hashicat-samuel" 
 }
